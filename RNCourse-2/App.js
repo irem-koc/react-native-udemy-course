e8134718900130aeb9b481bd4fbb7c9beedb2dd2
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-07 13:58:36 +0300

    Refactor App.js to include game logic and screens

--- RNCourse-2/App.js
+++ RNCourse-2/App.js
@@ -1,16 +1,42 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { ImageBackground, StyleSheet } from "react-native";
+import { useState } from "react";
+import { ImageBackground, SafeAreaView, StyleSheet } from "react-native";
+import Colors from "./constants/colors";
+import GameOverScreen from "./screens/GameOverScreen";
+import GameScreen from "./screens/GameScreen";
 import StartGameScreen from "./screens/StartGameScreen";
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+  const pickedNumberHandler = (number) => {
+    setUserNumber(number);
+    setGameIsOver(false);
+  };
+  let screen = <StartGameScreen onPick={pickedNumberHandler} />;
+  const gameOverHandler = () => {
+    setGameIsOver(true);
+  };
+  if (userNumber) {
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
+  }
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen />;
+  }
+
   return (
-    <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen}>
+    <LinearGradient
+      colors={["#ddb52f", Colors.primary500]}
+      style={styles.rootScreen}
+    >
       <ImageBackground
         source={require("./assets/images/background.png")}
         resizeMode="cover"
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        <StartGameScreen />
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
